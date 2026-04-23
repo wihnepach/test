07a9c09eb1f525e343d48d960b9a73049b74cd0b
wihnepach <mr8jaki@gmail.com>
2026-04-23 20:51:33 +0300

    chore: add server/app.js

---
+++ server/app.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+const env = require("../config/env");
+require("./db/database");
+
+const authRoutes = require("./routes/auth.routes");
+const tasksRoutes = require("./routes/tasks.routes");
+
+const app = express();
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(env.PUBLIC_DIR));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", tasksRoutes);
+
+app.get("/", (request, response) => {
+  response.sendFile(path.join(env.PUBLIC_DIR, "index.html"));
+});
+
+module.exports = app;
